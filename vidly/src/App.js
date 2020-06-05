f9c7468fea79342f2561afb112d7236d9ef8d3a3
jcammas <matoulet.cammas@gmail.com>
2020-06-05 23:56:38 +0200

    initial commit

---
+++ vidly/src/App.js
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+import "./App.css";
+import Movies from "./components/movies";
+import Customers from "./components/common/customers";
+import NotFound from "./components/common/notFound";
+import Rentals from "./components/common/rentals";
+import { Route, Redirect, Switch } from "react-router-dom";
+import NavBar from "./components/navBar";
+import MovieForm from "./components/common/movieForm";
+import LoginForm from "./components/loginForm";
+import RegisterForm from "./components/registerForm";
+import Logout from "./components/logout";
+import auth from "./services/authService";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import ProtectedRoute from "./components/common/protectedRoute";
+
+class App extends Component {
+  state = {};
+
+  componentDidMount() {
+    const user = auth.getCurrentUser();
+    this.setState({ user });
+  }
+
+  render() {
+    const { user } = this.state;
+
+    return (
+      <React.Fragment>
+        <ToastContainer />
+        <NavBar user={user} />
+        <main className="container">
+          <Switch>
+            <Route path="/register" component={RegisterForm} />
+            <Route path="/login" component={LoginForm} />
+            <Route path="/logout" component={Logout} />
+            <ProtectedRoute path="/movies/:id" component={MovieForm} />
+            <Route
+              path="/movies"
+              render={(props) => <Movies {...props} user={this.state.user} />}
+            />
+            <Route path="/customers" component={Customers}></Route>
+            <Route path="/rentals" component={Rentals}></Route>
+            <Route path="/not-found" component={NotFound}></Route>
+            <Redirect from="/" exact to="/movies" />
+            <Redirect to="/not-found" />
+          </Switch>
+        </main>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
